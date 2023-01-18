b276d786d7167b3ff232259b8b19163c23762db7
Lucas Almeida <lucasalmeida.1407@outlook.com>
2023-01-18 19:27:12 -0300

    Connecting to MongoDB with repository and service

---
+++ src/main/java/com/lucasalmeida/workshopmongodb/services/UserService.java
@@ -0,0 +1,21 @@
+package com.lucasalmeida.workshopmongodb.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lucasalmeida.workshopmongodb.domain.User;
+import com.lucasalmeida.workshopmongodb.repository.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+}
