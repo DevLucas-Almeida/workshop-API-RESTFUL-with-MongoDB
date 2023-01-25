5d87eb7dfe61cb627588e9839e7503628cf81da1
Lucas Almeida <lucasalmeida.1407@outlook.com>
2023-01-24 22:58:40 -0300

    Getting a user by id

--- src/main/java/com/lucasalmeida/workshopmongodb/services/UserService.java
+++ src/main/java/com/lucasalmeida/workshopmongodb/services/UserService.java
@@ -1,12 +1,14 @@
 package com.lucasalmeida.workshopmongodb.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.lucasalmeida.workshopmongodb.domain.User;
 import com.lucasalmeida.workshopmongodb.repository.UserRepository;
+import com.lucasalmeida.workshopmongodb.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -18,4 +20,8 @@ public class UserService {
 		return repository.findAll();
 	}
 	
+	public User findById(String id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+		}	
 }
