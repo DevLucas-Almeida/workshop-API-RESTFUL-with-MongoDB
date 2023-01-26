71c7d7d5d384020a1f623d3a87255a126bc687b2
Lucas Almeida <lucasalmeida.1407@outlook.com>
2023-01-26 13:51:14 -0300

    User insertion with POST

--- src/main/java/com/lucasalmeida/workshopmongodb/services/UserService.java
+++ src/main/java/com/lucasalmeida/workshopmongodb/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.lucasalmeida.workshopmongodb.domain.User;
+import com.lucasalmeida.workshopmongodb.dto.UserDto;
 import com.lucasalmeida.workshopmongodb.repository.UserRepository;
 import com.lucasalmeida.workshopmongodb.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
-		}	
+	}
+	
+	public User insert(User obj) {
+		return repository.insert(obj);
+	}
+	
+	public User fromDto(UserDto objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
 }
